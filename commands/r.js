d7e61ce1bb3084b410f04bd17ecf326e5479772e
Yumozi <xueeric1223@gmail.com>
2022-05-23 12:33:51 -0400

    Add base features for r.js with dice-roller

--- commands/r.js
+++ commands/r.js
@@ -1,14 +1,18 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { DiceRoll } = require('@dice-roller/rpg-dice-roller');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('r')
 		.setDescription('进行roll')
 		.addStringOption(option =>
-			option.setName('点数')
-				.setDescription('点数')
+			option.setName('类型')
+				.setDescription('类型')
 				.setRequired(true)),
 	async execute(interaction) {
-		await interaction.reply('是大失败!');
+		const rollType = interaction.options.getString('类型');
+		const roll = new DiceRoll(rollType);
+		// string[] /d/.split(rollType);
+		await interaction.reply(`骰子结果：${roll.toString()}`);
 	},
 };
